f31d70f2a91f8066bdac8e8d71bd3ffeeb2f04fa
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-09-11 21:19:37 -0300

    Hash password usuario bcrypt post Handler User

--- test/test.js
+++ test/test.js
@@ -7,8 +7,16 @@ chai.use(chai_http)
 const URI = 'http://127.0.0.1:' + config.PORT
 
 describe('ALL', () => {
-    it('EXAMPLE', (done) => {
-        chai.request(URI).post('/').end((err, res) => {
+    it('SHOULD INSERT USER', (done) => {
+			chai.request(URI).post('/').send({
+					User: {
+						Email: 'User@email.com',
+						Nombre: 'NOMBRE EJEMPLO',
+						Apellido: 'APELLIDO EJEMPLO',
+						Edad: 22,
+						Password: '123asc'
+					}
+			}).end((err, res) => {
             console.log(res)
             done()
         })
