91296ef3be3ce0f900360baad6dd52310b63595e
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-09-13 22:22:54 -0300

    Login JWT bcrypt compare

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ chai.use(chai_http)
 const URI = 'http://127.0.0.1:' + config.PORT
 
 describe('ALL', () => {
-    it('SHOULD INSERT USER', (done) => {
+  /*  it('SHOULD INSERT USER', (done) => {
 			chai.request(URI).post('/').send({
 					User: {
 						Email: 'User@email.com',
@@ -20,5 +20,19 @@ describe('ALL', () => {
             console.log(res)
             done()
         })
-    })
+	})
+*/
+
+	it('SHOULD LOGIN USER', (done) => {
+			chai.request(URI).post('/login').send({
+					User: {
+						Email: 'User@email.com',
+						Password: '123asc'
+					}
+			}).end((err, res) => {
+            console.log(res.body)
+            done()
+        })
+	})
+	
 })
